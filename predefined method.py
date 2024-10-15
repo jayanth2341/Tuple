0beba58839153d8c96fbebb706b2b6a401341564
jayanth2341 <yjayanth769@gmail.com>
2024-10-15 10:35:13 +0530

    predefined method.py

---
+++ predefined method.py
@@ -0,0 +1,10 @@
+"""
+Write a program to get n number of tuple elements from the user in separate lines and print the sum of their values using predefined method.
+Sample Input:
+3
+10
+20
+30
+Sample Output:
+60
+"""
