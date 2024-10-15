6a1b41155ff38d34f5114b58fea78531d629e9f2
jayanth2341 <yjayanth769@gmail.com>
2024-10-15 10:36:00 +0530

    maximum value of the given values.py

---
+++ maximum value of the given values.py
@@ -0,0 +1,10 @@
+"""
+Write a program to get n number of tuple elements from the user in separate line and print the maximum value of the given values.
+Sample Input:
+3
+20
+10
+30
+Sample Output:
+30
+"""
