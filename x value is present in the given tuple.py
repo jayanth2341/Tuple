65cbf65ce25024747ad274c32f289eb4a87b4f4f
jayanth2341 <yjayanth769@gmail.com>
2024-10-15 10:38:24 +0530

    x value is present in the given tuple.py

---
+++ x value is present in the given tuple.py
@@ -0,0 +1,8 @@
+"""
+Write a program to get the tuple values in a single line separated by space and count the nuber of times the given x value is present in the given tuple.
+Sample Input:
+1 2 3 1 2 3 4 1 2 1
+1
+Sample Output:
+4
+"""
